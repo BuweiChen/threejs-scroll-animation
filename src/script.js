460a375044d7665d8b8951a2b427b616bfebdcb9
Buwei Chen <59496117+BuweiChen@users.noreply.github.com>
2024-06-07 16:09:04 -0500

    add spin animations

--- src/script.js
+++ src/script.js
@@ -1,5 +1,6 @@
 import * as THREE from "three";
 import GUI from "lil-gui";
+import gsap from "gsap";
 
 /**
  * Debug
@@ -150,6 +151,13 @@ window.addEventListener("scroll", () => {
 
   if (newSection != currentSection) {
     currentSection = newSection;
+    gsap.to(sectionMeshes[currentSection].rotation, {
+      duration: 1.5,
+      ease: "power2.out",
+      x: "+=6",
+      y: "+=3",
+      z: "+=1.5",
+    });
   }
 });
 
@@ -176,8 +184,8 @@ const tick = () => {
 
   // Animate meshes
   for (const mesh of sectionMeshes) {
-    mesh.rotation.x = elapsedTime * 0.1;
-    mesh.rotation.y = elapsedTime * 0.12;
+    mesh.rotation.x += deltaTime * 0.1;
+    mesh.rotation.y += deltaTime * 0.12;
   }
 
   // Animate camera
